8f3a7e3ffd99f210f06ea15bd0119ffaddc588fa
khushi2497Patel <patelkhushboo03@gmail.com>
2018-12-31 10:56:18 +0530

    first commit

---
+++ fall_lib.py
@@ -0,0 +1,27 @@
+"""
+Fall detection algo
+"""
+
+def first_module(arg):
+    """
+    Doc string first module
+    """
+
+    print("first module created successfully")
+    return arg
+
+class First:
+
+    """
+    Doc string for class
+    """
+
+    def __init__(self):
+        print("Class created")
+
+    def first(self):
+        """
+        Doc string for class method 
+        """
+
+        print("method call")
